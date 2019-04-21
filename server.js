014898008143faa3fe2e73ea738e3ea80ce31599
Pankaj Tanwar Banna <2016ucp1381@mnit.ac.in>
2019-04-21 21:14:20 +0530

    Main.js added

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+
     console.log('A user connected.');
     socket.on('disconnect', function () {
         console.log('User disconnected.');
